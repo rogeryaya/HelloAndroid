d44a8595490355f5f226c66dfbe97113d0d65b68
Rogeryaya <rogerya@gmail.com>
2016-09-01 18:54:23 +0800

    Test
    Test Commit.

--- app/src/main/java/com/example/user/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/user/helloandroid/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("testLog", "this is a log");
+        Log.d("testLog", "this is a log.");
         Toast.makeText(MainActivity.this,
                 "this is a toast",
                 Toast.LENGTH_LONG).show();
